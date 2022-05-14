85bce0dc275183aad49a161f3159d61ad50be60f
MAHAM <abdulkabore@gmail.com>
2022-05-14 09:45:08 +0000

    first version of simple shell

--- main.h
+++ main.h
@@ -1,11 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-#define DICT_LEN 256
+#define BUFFSIZE 32
+#define BUFFER_LEN 1024
 
-pid_t spawnChild(char *buffer);
-int *create_delim_dict(char *delim);
-char *_strtok(char *str, char *delim);
+pid_t spawnChild(char **args_list);
+int fill_args(char *input, char **args_list);
+
+#endif
