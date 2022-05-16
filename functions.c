09079386b2eb6d72022efa31148a393e4bea6572
MAHAM <abdulkabore@gmail.com>
2022-05-16 17:52:52 +0200

    add our own strtok function

--- functions.c
+++ functions.c
@@ -109,3 +109,38 @@ int change_dir(const char *path)
 	}
 	return (1);
 }
+
+/**
+ * _strtok - Function that splits the string in tokens
+ * @str: a string to be splitted
+ * @separator: char separator
+ * Return: a token
+ */
+char *_strtok(char *str, const char *separator)
+{
+	static char *a;
+	char *token = NULL;
+
+	if (!str)
+	{
+		str = a;
+		if (!str)
+			return (NULL);
+	}
+	str += strspn(str, separator);
+	if (!*str)
+	{
+		a = NULL;
+		return (NULL);
+	}
+	token = str;
+	str = strpbrk(token, separator);
+	if (str)
+	{
+		*str = '\0';
+		a = str + 1;
+	}
+	else
+		a = NULL;
+	return (token);
+}
