c9358b3e0279e69cbb399b880d743b453927305c
MAHAM <abdulkabore@gmail.com>
2022-05-19 12:14:48 +0000

    update tools1.c

--- tools1.c
+++ tools1.c
@@ -6,6 +6,25 @@
 #include "main.h"
 
 /**
+ * prompt - Prints the prompt in the standar input
+ * @prompt: Pointer to the string to be printed
+ * @size: Length ot the prompt
+ * Return: 0 if success or -1 if fails
+ */
+int prompt(const char *prompt, unsigned int size)
+{
+	int written;
+
+	if (isatty(STDIN_FILENO))
+	{
+		written = write(1, prompt, size);
+		if (written == -1)
+			return (-1);
+	}
+	return (0);
+}
+
+/**
  * _strlen - Sizes the length of a string
  * @str: String to be sized
  * Return: The length ot the string
